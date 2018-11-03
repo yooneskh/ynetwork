e5853bdb1cf653f09aa2b0a34094f2b6e43f44b8
Yoones Khoshghadam <yoonskh@gmail.com>
2018-11-03 13:11:47 +0330

    bugfix on dismiss

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = {
 
             let preProcessorResult = this.preProcessor ? this.preProcessor(res.data, res.status) : null;
 
-            if (!preProcessorResult) return console.log('ynetwork dismissed', method, url);
+            if (this.preProcessor && !preProcessorResult) return console.log('ynetwork dismissed', method, url);
 
             if (typeof preProcessorResult === 'object') {
                 res.data   = preProcessorResult.data;
@@ -55,7 +55,7 @@ module.exports = {
 
             let preProcessorResult = this.preProcessor ? this.preProcessor(_response, _status) : null;
 
-            if (!preProcessorResult) return console.log('ynetwork dismissed error', method, url);
+            if (this.preProcessor && !preProcessorResult) return console.log('ynetwork dismissed error', method, url);
 
             if (typeof preProcessorResult === 'object') {
                 _response = preProcessorResult.data;
